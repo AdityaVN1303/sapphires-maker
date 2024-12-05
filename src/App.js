35d91283ac4cbc727891770498f41d4d16cd9315
Aditya Nagare <adityanagare09@gmail.com>
2024-12-05 10:07:24 +0530

    added final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
-function App() {
+const App = () => {
+  const [active, setActive] = useState(function () {
+    return JSON.parse(localStorage.getItem("activeIdx"));
+  });
+  const [locked, setLocked] = useState(true);
+
+  function handleClick(index) {
+    if (locked) {
+      return;
+    }
+    localStorage.setItem("activeIdx", index?.toString());
+    setActive(index);
+    setLocked(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="above">
+      <div className="main grid grid-cols-5 px-1 items-center text-center">
+        {Array.from({ length: 160 }, (_, index) => {
+          // Customize the element based on the index
+          return (
+            <div
+              key={index}
+              onClick={() => {
+                handleClick(index);
+              }}
+              className={`p-3 border border-black cursor-pointer flex justify-center items-center ${
+                index <= active && "bg-red-700 text-white"
+              } ${index === active && "bg-blue-600"}`}
+            >
+              {index + 1}
+            </div>
+          );
+        })}
+      </div>
+      <button
+        onClick={() => {
+          setLocked((val) => !val);
+        }}
+        className="p-5 bg-blue-600 text-white w-full text-xl text-center font-bold"
+      >
+        {locked ? "Locked 🔒" : "Unlocked 🔓"}
+      </button>
     </div>
   );
-}
+};
 
 export default App;
